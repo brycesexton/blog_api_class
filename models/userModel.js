bcf64dc241c8ee3c7730ef0028737c0d53e8ee6e
Bryce Sexton <brycesexton15@gmail.com>
2024-02-20 16:42:59 -0800

    fixed userModel and added jwt stuff

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
+require ('dotenv').config()
 const { model, Schema } = require('mongoose')
-
+const bcrypt = require ('bcrypt')
+const jwt = require ('jsonwebtoken')
 
 const userSchema = new Schema ({
     name: { required: true, type: String },
@@ -10,6 +12,19 @@ const userSchema = new Schema ({
 }, {
     timestamps: true
 })
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next()
+})
+
+userSchema.methods.generateAuthToken = async function() {
+    const secretKey = process.env.JWT_SECRET
+    const token = jwt.sign({ _id: this._id }, secretKey)
+    return token
+}
 
 const User = model('User', userSchema)
 
