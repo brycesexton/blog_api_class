c1f3f78c6653f9e9e3bd24e28bb078cc220e1d26
Bryce Sexton <brycesexton15@gmail.com>
2024-02-20 17:33:11 -0800

    changed app-server to reflect blogs

--- app-server.js
+++ app-server.js
@@ -4,17 +4,18 @@ const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
 
-
-app.use(express.json()) // req.body
+app.use(express.json())
 app.use((req, res, next) => {
     res.locals.data = {}
     next()
 })
+
 app.use(logger('dev'))
 app.use(favicon(path.join(__dirname, 'public', 'img','logo.png')))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/api/todos', require('./routes/api/todos'))
-// http://localhost:8000/api/todos/completed
+app.use('/api/blogs', require('./routes/api/blogRouter'))
+app.use('/api/users', require('./routes/api/userRouter'))
+// http://localhost:8000/api/blogs
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
