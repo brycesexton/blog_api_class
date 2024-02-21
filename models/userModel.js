f6e7553335c19697e27a3461b5ad08fddb8e24e9
Bryce Sexton <brycesexton15@gmail.com>
2024-02-20 16:09:09 -0800

    added userSchema

---
+++ models/userModel.js
@@ -0,0 +1,16 @@
+const { model, Schema } = require('mongoose')
+
+
+const userSchema = new Schema ({
+    name: { required: true, type: String },
+    email: { required: true, type: String },
+    password: { required: true, type: String },
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Blog' }],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+}, {
+    timestamps: true
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
