d1f9051c53feed3ab885f5b9d128e9998fdaa868
Bryce Sexton <brycesexton15@gmail.com>
2024-02-21 18:49:56 -0800

    changed index.js to include react router dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+//before react router code
+// import {StrictMode} from "react"
+// import { createRoot } from "react-dom/client"
+// import App from './App'
+// const root = createRoot(document.getElementById("app"))
+// root.render(<StrictMode><App/></StrictMode>)
+
 import {StrictMode} from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom" //new + <router>
 import App from './App'
 const root = createRoot(document.getElementById("app"))
-root.render(<StrictMode><App/></StrictMode>)
+root.render(
+    <StrictMode>
+        <Router> 
+            <App/>
+        </Router>
+    </StrictMode>)
